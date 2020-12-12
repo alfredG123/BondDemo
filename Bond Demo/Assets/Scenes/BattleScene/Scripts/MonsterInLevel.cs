ccdd47070f165e5c2569d82099bb4d7fbfc015cd
Alfred'sGame <game501294163@gmail.com>
2020-12-11 22:28:03 -0500

    Make a scriptobject for defining monsters in a level.

---
+++ Bond Demo/Assets/Scenes/BattleScene/Scripts/MonsterInLevel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Level", menuName ="BOND/Level")]
+public class MonsterInLevel : ScriptableObject
+{
+    public int game_level;
+    public List<MonsterData> monsters_in_level;
+}
