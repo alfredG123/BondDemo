6b22dc3d3e2b597b3e138ca2d6d4ed1117aae3ff
Alfred'sGame <game501294163@gmail.com>
2020-12-18 21:43:54 -0500

    Improve the scripts for the maze generation.

--- Bond Demo/Assets/Scenes/BattleScene/Scripts/Maze/MazeManagement.cs
+++ Bond Demo/Assets/Scenes/BattleScene/Scripts/Maze/MazeManagement.cs
@@ -18,7 +18,7 @@ public class MazeManagement : MonoBehaviour
     private GameObject player;
     private List<GameObject> all_rooms;
     private GameObject end;
-    private GameObject starting_room;
+    private GameObject current_room;
 
     private void Awake()
     {
@@ -26,9 +26,9 @@ public class MazeManagement : MonoBehaviour
 
         int rand = UnityEngine.Random.Range(0, possible_starting_position.Count);
         
-        starting_room = GameObject.Instantiate(possible_starting_position[rand], Vector2.zero, Quaternion.identity);
-        starting_room.transform.SetParent(map.transform);
-        starting_room.GetComponent<Room>().GenerateNeighbors();
+        current_room = GameObject.Instantiate(possible_starting_position[rand], Vector2.zero, Quaternion.identity);
+        current_room.transform.SetParent(map.transform);
+        current_room.GetComponent<Room>().GenerateNeighbors();
 
         player = GameObject.Instantiate(start_position_icon, possible_starting_position[rand].transform.position, Quaternion.identity);
         player.transform.SetParent(map.transform);
@@ -52,23 +52,82 @@ public class MazeManagement : MonoBehaviour
             }
         }
 
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Collider2D game_object_detector;
-        //    Vector2 mouse_position = GeneralScripts.GetMousePositionInWorldSpace();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D game_object_detector;
+            Vector2 mouse_position = GeneralScripts.GetMousePositionInWorldSpace();
+            GameObject room;
 
-        //    game_object_detector = Physics2D.OverlapCircle(mouse_position, 0.1f);
+            game_object_detector = Physics2D.OverlapCircle(mouse_position, 0.1f);
 
-        //    if (game_object_detector != null)
-        //    {
-        //        float distance = Vector2.Distance(player.transform.localPosition, game_object_detector.gameObject.transform.localPosition);
+            if (game_object_detector != null)
+            {
+                float distance = Vector2.Distance(player.transform.localPosition, game_object_detector.gameObject.transform.localPosition);
 
-        //        if (distance <= 2f)
-        //        {
-        //            player.transform.localPosition = game_object_detector.gameObject.transform.localPosition;
-        //        }
-        //    }
-        //}
+                if (distance <= 2f)
+                {
+                    room = game_object_detector.gameObject;
+
+                    if (current_room.GetComponent<Room>().CheckIsNeighbor(room))
+                    {
+                        if (current_room.GetComponent<Room>().GetMystery())
+                        {
+                            current_room.SetActive(false);
+                        }
+
+                        current_room = room;
+
+                        player.transform.localPosition = game_object_detector.gameObject.transform.localPosition;
+                    }
+                    else
+                    {
+                        TypeDoor direction;
+
+                        float x = player.transform.localPosition.x - game_object_detector.gameObject.transform.localPosition.x;
+                        float y = player.transform.localPosition.y - game_object_detector.gameObject.transform.localPosition.y;
+
+                        GameObject mystery_room;
+
+                        if (x > 0)
+                        {
+                            direction = TypeDoor.LeftDoor;
+                            mystery_room = base_room[3];
+                        }
+                        else if(x < 0)
+                        {
+                            direction = TypeDoor.RightDoor;
+                            mystery_room = base_room[2];
+                        }
+                        else if (y > 0)
+                        {
+                            direction = TypeDoor.BottomDoor;
+                            mystery_room = base_room[0];
+                        }
+                        else
+                        {
+                            direction = TypeDoor.TopDoor;
+                            mystery_room = base_room[1];
+                        }
+
+                        if (current_room.GetComponent<Room>().NeedMysteryRoom(direction))
+                        {
+                            GameObject visited_mystery_room = current_room.GetComponent<Room>().GetMysteryRoom(direction);
+
+                            if (visited_mystery_room == null)
+                            {
+                                current_room = current_room.GetComponent<Room>().CreateMysterRoom(direction, mystery_room);
+                            }
+                            else
+                            {
+                                current_room = visited_mystery_room;
+                            }
+
+                            player.transform.localPosition = game_object_detector.gameObject.transform.localPosition;
+                        }
+                    }
+                }
+            }
+        }
 
         //if (Input.GetMouseButtonDown(1))
         //{
--- Bond Demo/Assets/Scenes/BattleScene/Scripts/Maze/Room.cs
+++ Bond Demo/Assets/Scenes/BattleScene/Scripts/Maze/Room.cs
@@ -10,6 +10,8 @@ public class Room : MonoBehaviour
 
     private List<GameObject> neighbors = new List<GameObject>();
     private float distance = 2f;
+    private GameObject map;
+    private bool is_mystery = false;
 
     public void GenerateNeighbors()
     {
@@ -17,7 +19,7 @@ public class Room : MonoBehaviour
         GameObject room_to_create = null;
         Vector2 position = Vector2.zero;
 
-        GameObject map = GameObject.Find("Map");
+        map = GameObject.Find("Map");
 
         MazeManagement maze_manager = GameObject.Find("MazeManagement").GetComponent<MazeManagement>();
         RoomList room_list = maze_manager.GetRoomList();
@@ -54,14 +56,12 @@ public class Room : MonoBehaviour
             {
                 neighbor = GameObject.Instantiate(room_to_create, position, Quaternion.identity);
                 neighbor.transform.SetParent(map.transform);
+                neighbor.GetComponent<Room>().GenerateNeighbors();
+
+                neighbor.GetComponent<Room>().SetNeighbor(gameObject);
 
                 neighbors.Add(neighbor);
             }
-        }
-
-        foreach (GameObject the_neighbor in neighbors)
-        {
-            the_neighbor.GetComponent<Room>().GenerateNeighbors();
         }
     }
 
@@ -77,5 +77,123 @@ public class Room : MonoBehaviour
         }
 
         return (is_valid);
+    }
+
+    public bool CheckIsNeighbor(GameObject room)
+    {
+        bool is_neighbor = false;
+
+        if (neighbors.Contains(room))
+        {
+            is_neighbor = true;
+        }
+
+        return (is_neighbor);
+    }
+
+    public void SetNeighbor(GameObject room)
+    {
+        neighbors.Add(room);
+    }
+
+    public bool NeedMysteryRoom(TypeDoor door)
+    {
+        bool need = false;
+
+        if (doors.Contains(door))
+        {
+            need = true;
+        }
+
+        return (need);
+    }
+
+    // for myster only
+    public TypeDoor GetDoor()
+    {
+        return (doors[0]);
+    }
+
+    public GameObject GetMysteryRoom(TypeDoor door)
+    {
+        GameObject room = null;
+        TypeDoor mystery_room = TypeDoor.TopDoor;
+
+        foreach (GameObject neighbor in neighbors)
+        {
+            switch (door)
+            {
+                case TypeDoor.TopDoor:
+                    mystery_room = TypeDoor.BottomDoor;
+                    break;
+
+                case TypeDoor.BottomDoor:
+                    mystery_room = TypeDoor.TopDoor;
+                    break;
+
+                case TypeDoor.LeftDoor:
+                    mystery_room = TypeDoor.RightDoor;
+                    break;
+
+                case TypeDoor.RightDoor:
+                    mystery_room = TypeDoor.LeftDoor;
+                    break;
+            }
+
+            if ((neighbor.GetComponent<Room>().GetMystery()) && (neighbor.GetComponent<Room>().GetDoor() == mystery_room))
+            {
+                room = neighbor;
+                neighbor.SetActive(true);
+                break;
+            }
+        }
+
+        return (room);
+    }
+
+    public bool GetMystery()
+    {
+        return (is_mystery);
+    }
+
+    public void SetMystery()
+    {
+        is_mystery = true;
+
+        GetComponent<SpriteRenderer>().sortingLayerName = "MysteryRoom";
+    }
+
+    public GameObject CreateMysterRoom(TypeDoor door, GameObject room_to_create)
+    {
+        Vector2 position = Vector2.zero;
+
+        switch (door)
+        {
+            case TypeDoor.TopDoor:
+                position = new Vector2(transform.position.x, transform.position.y + distance);
+                break;
+
+            case TypeDoor.BottomDoor:
+                position = new Vector2(transform.position.x, transform.position.y - distance);
+                break;
+
+            case TypeDoor.LeftDoor:
+                position = new Vector2(transform.position.x - distance, transform.position.y);
+                break;
+
+            case TypeDoor.RightDoor:
+                position = new Vector2(transform.position.x + distance, transform.position.y);
+                break;
+        }
+
+        GameObject room = GameObject.Instantiate(room_to_create, position, Quaternion.identity);
+        room.transform.SetParent(map.transform);
+
+        room.GetComponent<Room>().SetNeighbor(gameObject);
+        room.GetComponent<Room>().SetMystery();
+
+        neighbors.Add(room);
+
+        return (room);
     }
 }
