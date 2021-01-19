b675f945c67d4e57b48719cfb673adc724c8e3bf
Alfred'sGame <game501294163@gmail.com>
2021-01-18 20:21:44 -0500

    Continued to modify the structure.

--- Bond Demo/Assets/General/Scripts/Enumeration/BaseSpirit.cs
+++ Bond Demo/Assets/General/Scripts/Enumeration/BaseSpirit.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 public class BaseSpirit : BaseEnumeration
 {
-    public static BaseSpirit A1 = new BaseSpirit(0, "A1", 39f, 52f, 43f, 60f, 50f, 65f, new TypeAttribute[] { TypeAttribute.Water, TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Fire, TypeAttribute.Plant }, new TypeAttribute[] { });
-    public static BaseSpirit B1 = new BaseSpirit(1, "B1", 70f, 40f, 50f, 55f, 50f, 25f, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Plant, TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Water }, new TypeAttribute[] { });
-    public static BaseSpirit C1 = new BaseSpirit(2, "C1", 50f, 50f, 77f, 95f, 77f, 91f, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Wind }, new TypeAttribute[] { });
-    public static BaseSpirit D1 = new BaseSpirit(3, "D1", 68f, 72f, 78f, 38f, 42f, 32f, new TypeAttribute[] { TypeAttribute.Water, TypeAttribute.Plant }, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric });
-    public static BaseSpirit E1 = new BaseSpirit(4, "E1", 40f, 38f, 35f, 54f, 35f, 40f, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Plant, TypeAttribute.Wind }, new TypeAttribute[] { });
+    public static BaseSpirit A1 = new BaseSpirit(0, "A1", "SpiritA1", 39f, 52f, 43f, 65f, new TypeAttribute[] { TypeAttribute.Water, TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Fire, TypeAttribute.Plant }, new TypeAttribute[] { });
+    public static BaseSpirit B1 = new BaseSpirit(1, "B1", "SpiritB1", 70f, 40f, 50f, 25f, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Plant, TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Water }, new TypeAttribute[] { });
+    public static BaseSpirit C1 = new BaseSpirit(2, "C1", "SpiritC1", 50f, 50f, 77f, 91f, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Wind }, new TypeAttribute[] { });
+    public static BaseSpirit D1 = new BaseSpirit(3, "D1", "SpiritD1", 68f, 72f, 78f, 32f, new TypeAttribute[] { TypeAttribute.Water, TypeAttribute.Plant }, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric });
+    public static BaseSpirit E1 = new BaseSpirit(4, "E1", "SpiritE1", 40f, 38f, 35f, 40f, new TypeAttribute[] { TypeAttribute.Earth }, new TypeAttribute[] { TypeAttribute.Electric, TypeAttribute.Plant, TypeAttribute.Wind }, new TypeAttribute[] { });
 
     private readonly float _critical_chance = .05f;
     private readonly float _evasion = 0.01f;
@@ -15,21 +15,34 @@ public class BaseSpirit : BaseEnumeration
 
     private readonly List<TypeAttribute> _weakness = null;
     private readonly List<TypeAttribute> _resistance = null;
-    private readonly List<TypeAttribute> _perfect_guard = null;
+    private readonly List<TypeAttribute> _negation = null;
 
-    public BaseSpirit(int id, string name, float health, float physicalAttack, float physicalDefense, float etherAttack, float etherDefense, float speed, TypeAttribute[] weakness, TypeAttribute[] resistance, TypeAttribute[] perfect_guard)
+    /// <summary>
+    /// Default Constructor
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    /// <param name="image_name"></param>
+    /// <param name="health"></param>
+    /// <param name="attack"></param>
+    /// <param name="defense"></param>
+    /// <param name="speed"></param>
+    /// <param name="weakness"></param>
+    /// <param name="resistance"></param>
+    /// <param name="negation"></param>
+    public BaseSpirit(int id, string name, string image_name, float health, float attack, float defense, float speed, TypeAttribute[] weakness, TypeAttribute[] resistance, TypeAttribute[] negation)
         : base(id, name)
     {
+        ImageName = image_name;
+
         Health = health;
-        PhysicalAttack = physicalAttack;
-        PhysicalDefense = physicalDefense;
-        EtherAttack = etherAttack;
-        EtherDefense = etherDefense;
+        Attack = attack;
+        Defense = defense;
         Speed = speed;
 
         _weakness = new List<TypeAttribute>();
         _resistance = new List<TypeAttribute>();
-        _perfect_guard = new List<TypeAttribute>();
+        _negation = new List<TypeAttribute>();
 
         for (int i = 0; i < weakness.Length; i++)
         {
@@ -41,25 +54,32 @@ public class BaseSpirit : BaseEnumeration
             _resistance.Add(resistance[i]);
         }
 
-        for (int i = 0; i < perfect_guard.Length; i++)
+        for (int i = 0; i < negation.Length; i++)
         {
-            _perfect_guard.Add(resistance[i]);
+            _negation.Add(resistance[i]);
         }
     }
 
     #region Properties
+    public string ImageName { get; }
+
     public float Health { get; }
 
-    public float PhysicalAttack { get; }
+    public float Attack { get; }
 
-    public float PhysicalDefense { get; }
-
-    public float EtherAttack { get; }
-
-    public float EtherDefense { get; }
+    public float Defense { get; }
 
     public float Speed { get; }
     #endregion
+
+    /// <summary>
+    /// Return the file path for Resources.Load
+    /// </summary>
+    /// <returns></returns>
+    public string GetImageNameWithPath()
+    {
+        return ("Spirits/" + ImageName);
+    }
 
     public bool CheckCriticalHit()
     {
@@ -120,7 +140,7 @@ public class BaseSpirit : BaseEnumeration
             effectiveness = TypeEffectiveness.NotEffective;
         }
 
-        if ((effectiveness == TypeEffectiveness.Effective) && (_perfect_guard.Contains(attack_move_attribute)))
+        if ((effectiveness == TypeEffectiveness.Effective) && (_negation.Contains(attack_move_attribute)))
         {
             effectiveness = TypeEffectiveness.NoEffect;
         }
--- Bond Demo/Assets/Scenes/PartnerSelection/Scripts/Systems/PartnerSelectionUIDisplay.cs
+++ Bond Demo/Assets/Scenes/PartnerSelection/Scripts/Systems/PartnerSelectionUIDisplay.cs
@@ -1,233 +1,104 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PartnerSelectionUIDisplay : MonoBehaviour
 {
+    [SerializeField] GameObject _SelectionPanel = null;
+    [SerializeField] GameObject _DetailPanel = null;
+
+    [SerializeField] GameObject _SpiritImage = null;
+    [SerializeField] GameObject _DetailTable = null;
+
+    private void Awake()
+    {
+        _SelectionPanel.SetActive(true);
+    }
+
     /// <summary>
     /// Display UI to show info about the selected spirit
     /// </summary>
     /// <param name="spirit_index"></param>
     public void DisplaySelectedSpiritInfo(int spirit_index)
     {
-        DisplaySpiritInfo(spirit_index);
+        BaseSpirit spirit = null;
 
-        // Show buttons for confirming or reconsidering
-        //DisplayConfirmation();
+        if (spirit_index == 0)
+        {
+            spirit = BaseSpirit.A1;
+        }
+        else if (spirit_index == 1)
+        {
+            spirit = BaseSpirit.B1;
+        }
+        else if (spirit_index == 2)
+        {
+            spirit = BaseSpirit.C1;
+        }
+        else if (spirit_index == 3)
+        {
+            spirit = BaseSpirit.D1;
+        }
+        else if (spirit_index == 4)
+        {
+            spirit = BaseSpirit.E1;
+        }
+
+        DisplaySpiritInfo(spirit);
+
+        SetSelectionDisplay(false);
+
+        SetDetailPanelDisplay(true);
     }
 
     /// <summary>
-    /// Display UI for showing related information about the chosen spirit
+    /// Modified the image and text for showing related information about the chosen spirit
     /// </summary>
-    /// <param name="spirit_index"></param>
-    private void DisplaySpiritInfo(int spirit_index)
+    /// <param name="spirit"></param>
+    private void DisplaySpiritInfo(BaseSpirit spirit)
     {
-        
+        _SpiritImage.GetComponent<Image>().sprite = Resources.Load<Sprite>(spirit.GetImageNameWithPath());
+
+        _DetailTable.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Name: " + spirit.Name;
+        _DetailTable.transform.GetChild(1).gameObject.GetComponent<Text>().text = "Health: " + spirit.Health;
+        _DetailTable.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Attack: " + spirit.Attack;
+        _DetailTable.transform.GetChild(3).gameObject.GetComponent<Text>().text = "Defense: " + spirit.Defense;
+        _DetailTable.transform.GetChild(4).gameObject.GetComponent<Text>().text = "Speed: " + spirit.Speed;
     }
 
-    // Display UI for showing related information about the chosen spirit
-    private void DisplaySpiritInfo()
-    {
-
-    }
-
-#if OLD
-
-    private void Start()
-    {
-        // Generate three random starting spirits
-        SetUpStarterSpiritList();
-    }
-
-
-    private void Update()
-    {
-        // Listen the mouse click, if no spirit is selected
-        if (_is_spirit_choose)
-        {
-
-            // Check if the left mouse is clicked
-            if (Input.GetMouseButtonDown(0))
-            {
-                // Check collision at the mouse position
-                _starter_spirit_object = General.GetGameObjectAtMousePosition();
-
-                if (_starter_spirit_object != null)
-                {
-                    // Error handling (There should only be spirit objects in the scene)
-                    if (_starter_spirit_object.GetComponent<SpiritPrefab>() == null)
-                    {
-                        General.ReturnToTitleSceneForErrors("StarterManagement.Update", "starter_spirit does not have SpiritPrefab script.");
-                    }
-
-                    // Display the relation information and confirmation
-                    ChooseSpirit();
-                }
-            }
-
-            // Check if the number key is clicked
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                // Set the left spirit as the starter
-                _starter_spirit_object = start_spirit1;
-
-                // Display the relation information and confirmation
-                ChooseSpirit();
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                // Set the center spirit as the starter
-                _starter_spirit_object = start_spirit2;
-
-                // Display the relation information and confirmation
-                ChooseSpirit();
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                // Set the right spirit as the starter
-                _starter_spirit_object = start_spirit3;
-
-                // Display the relation information and confirmation
-                ChooseSpirit();
-            }
-        }
-        else
-        {
-            // Check if the key is clicked
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                // Load the next scene
-                ConfirmChoice();
-            }
-            else if (Input.GetKeyDown(KeyCode.Backspace))
-            {
-                // Show the UI and reset global
-                RegretChoice();
-            }
-        }
-    }
-
-
-    // Handle the onclick event for reconsidering
-    public void RegretChoice()
-    {
-        // Hide confirmation related UI
-        HideStatsAndConfirmation();
-
-        // Reset the global flag for enbling the selection
-        _is_spirit_choose = true;
-    }
-
-    // Handle the onclick event for confirming
+    /// <summary>
+    /// If the decision is confirmed, load the main scene
+    /// </summary>
     public void ConfirmChoice()
     {
-        // Add the chosen spirit to the party
-        game_management.GetComponent<PlayerManagement>().AddSpiritToParty(_starter_spirit_object.GetComponent<SpiritPrefab>().Spirit);
-
         // Load the main scene 
         General.LoadScene(TypeScene.Main);
     }
 
-    // Randomly pick three starters
-    private void SetUpStarterSpiritList()
+    /// <summary>
+    /// If the decision is unconfirmed, change the panel
+    /// </summary>
+    public void BackButtonEvent()
     {
-        int rand;
-        int starter_index;
-        List<int> temp_index_list = new List<int>();
-        List<int> starter_spirits_index_list = new List<int>();
+        SetDetailPanelDisplay(false);
 
-        // Initialize a temporary list to hold the spirit index
-        for (int i = 0; i < starter_spirits.NumberOfSpirits; i++)
-        {
-            temp_index_list.Add(i);
-        }
-
-        // Pick three indexes from the temporary list
-        while (starter_spirits_index_list.Count < 3)
-        {
-            rand = Random.Range(0, temp_index_list.Count);
-
-            starter_index = temp_index_list[rand];
-
-            starter_spirits_index_list.Add(starter_index);
-
-            temp_index_list.RemoveAt(rand);
-        }
-
-
-        // initialize a spirit object for each prefab
-
-
-        Spirit spirit = new Spirit(starter_spirits.GetSpiritData(starter_spirits_index_list[0]));
-        start_spirit1.GetComponent<SpiritPrefab>().SetSpirit(spirit);
-
-        spirit = new Spirit(starter_spirits.GetSpiritData(starter_spirits_index_list[1]));
-        start_spirit2.GetComponent<SpiritPrefab>().SetSpirit(spirit);
-
-        spirit = new Spirit(starter_spirits.GetSpiritData(starter_spirits_index_list[2]));
-        start_spirit3.GetComponent<SpiritPrefab>().SetSpirit(spirit);
+        SetSelectionDisplay(true);
     }
 
-    // Display UI for selecting a spirit
-    private void ChooseSpirit()
+    /// <summary>
+    /// Set the visibility for the panel
+    /// </summary>
+    /// <param name="is_visible"></param>
+    private void SetDetailPanelDisplay(bool is_visible)
     {
-        // Show the UI that contains information about the chosen spirit
-        DisplaySpiritInfo();
-
-        // Show buttons for confirming or reconsidering
-        DisplayConfirmation();
-
-        _is_spirit_choose = false;
+        _DetailPanel.SetActive(is_visible);
     }
 
-    // Display UI for showing related information about the chosen spirit
-    private void DisplaySpiritInfo()
+    /// <summary>
+    /// Set the visibility for the panel
+    /// </summary>
+    /// <param name="is_visible"></param>
+    private void SetSelectionDisplay(bool is_visible)
     {
-        // Place the UI at the pre-defined location that is specified by the first child of the spirit prefab object
-        Vector3 position = General.ConvertWorldToScreenPosition(_starter_spirit_object.transform.GetChild(0).transform.position);
-
-        stats_details.transform.position = position;
-
-        // Calculate the pivot to fit the UI in the screen
-        float pivot_x = position.x / Screen.width;
-        float pivot_y = position.y / Screen.height;
-
-        stats_details_rect_transform.pivot = new Vector2(pivot_x, pivot_y);
-
-
-        // Set the text in the UI
-        
-        
-        Spirit starter_spirit = _starter_spirit_object.GetComponent<SpiritPrefab>().Spirit;
-
-        // Modified the text objects to show stats
-        stats_details.transform.GetChild(0).gameObject.GetComponent<Text>().text = "Name: " + starter_spirit.SpiritName;
-        stats_details.transform.GetChild(1).gameObject.GetComponent<Text>().text = "Health: " + starter_spirit.HealthText;
-        stats_details.transform.GetChild(2).gameObject.GetComponent<Text>().text = "Attack: " + starter_spirit.AttackText;
-        stats_details.transform.GetChild(3).gameObject.GetComponent<Text>().text = "Defense: " + starter_spirit.DefenseText;
-        stats_details.transform.GetChild(4).gameObject.GetComponent<Text>().text = "Speed: " + starter_spirit.SpeedText;
-
-        // Display basic moves
-
-        // Display the UI
-        stats_details.SetActive(true);
+        _SelectionPanel.SetActive(is_visible);
     }
-
-    // Activate UI that are related to the confirmation
-    private void DisplayConfirmation()
-    {
-        instruction_text.SetActive(false);
-
-        confirmation_dialogue.SetActive(true);
-    }
-
-    // Deactivate UI that are related to the confirmation
-    private void HideStatsAndConfirmation()
-    {
-        stats_details.SetActive(false);
-
-        confirmation_dialogue.SetActive(false);
-
-        instruction_text.SetActive(true);
-    }
-#endif
-
 }
