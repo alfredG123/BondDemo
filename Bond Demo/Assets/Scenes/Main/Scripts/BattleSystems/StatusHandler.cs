f8588116e74d6eda706bbffb0f3f43b74047c57a
Alfred'sGame <game501294163@gmail.com>
2021-01-30 21:47:48 -0500

    Added scripts for setting the status display.

---
+++ Bond Demo/Assets/Scenes/Main/Scripts/BattleSystems/StatusHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusHandler : MonoBehaviour
+{
+    private GameObject _NameObject = null;
+    private GameObject _LevelObject = null;
+    private GameObject _HealthObject = null;
+    private GameObject _StaminaObject = null;
+
+    public void InitializeStatus(Spirit spirit)
+    {
+        _NameObject = transform.GetChild(0).gameObject;
+        _NameObject.GetComponent<Text>().text = spirit.Name;
+
+        _LevelObject = transform.GetChild(1).gameObject;
+        _LevelObject.GetComponent<Text>().text = "Lv." + spirit.Level.ToString();
+
+        _HealthObject = transform.GetChild(2).gameObject;
+        _HealthObject.GetComponent<Slider>().value = 1;
+
+        _StaminaObject = transform.GetChild(3).gameObject;
+        _StaminaObject.GetComponent<Slider>().value = 1;
+
+        gameObject.SetActive(true);
+    }
+}
